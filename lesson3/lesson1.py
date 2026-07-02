3e85c7bcf9bd6624e0cbc5fa714c7f450d7f0124
rishit <rishitsaharia@gmail.com>
2026-07-02 17:11:15 -0400

    rishitsaharia@gmail.com

---
+++ lesson3/lesson1.py
@@ -0,0 +1,22 @@
+print("My Daily Routine")
+
+# Step 1: Waking up:
+print("I wake up at 6:00 AM.")
+
+# Step 2: Breakfast:
+print("I have a breakfast of sandwiches")
+
+# Step 3: Going to school:
+print("I leave for school at 7:30 AM.")
+
+# Step 4: Attending classes:
+print("I attend school from 8:00 AM to 3:00 PM.")
+
+# Step 5: Going home:
+print("I return home at 3:30 PM.")
+
+# Step 6: Homework:
+print("I study up for next year's classes and do my homework from 4:00 PM to 6:00 PM.")
+
+# calculating total hours spent on studying and homework weekly:
+print("Total hours spent studying in a week: ", 2 * 7)
