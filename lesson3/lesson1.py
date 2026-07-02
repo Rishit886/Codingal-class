919a8051d834981102c677aec04161185d0e1e1c
rishit <rishitsaharia@gmail.com>
2026-07-02 17:22:18 -0400

    Fix indentation for comments in daily routine script

--- lesson3/lesson1.py
+++ lesson3/lesson1.py
@@ -1,21 +1,21 @@
 print("My Daily Routine")
 
-# Step 1: Waking up:
+ # Step 1: Waking up:
 print("I wake up at 6:00 AM.")
 
-# Step 2: Breakfast:
+ # Step 2: Breakfast:
 print("I have a breakfast of sandwiches")
 
-# Step 3: Going to school:
+ # Step 3: Going to school:
 print("I leave for school at 7:30 AM.")
 
-# Step 4: Attending classes:
+ # Step 4: Attending classes:
 print("I attend school from 8:00 AM to 3:00 PM.")
 
-# Step 5: Going home:
+ # Step 5: Going home:
 print("I return home at 3:30 PM.")
 
-# Step 6: Homework:
+ # Step 6: Homework:
 print("I study up for next year's classes and do my homework from 4:00 PM to 6:00 PM.")
 
 # calculating total hours spent on studying and homework weekly:
