14446338e37f35ef2cb1a65de39e3384d6288c57
rishit <rishitsaharia@gmail.com>
2026-07-02 12:43:12 -0400

    lesson2

---
+++ lesson2/activity1.py
@@ -0,0 +1,8 @@
+print("Hello Rishit! Welcome to Codingal!")
+a="9"
+b=9
+print(type(a))
+print(type(b))
+print("9")
+print(9)
+print("Welcome to Codingal", end="*")
---
+++ lesson2/activity3.py
@@ -0,0 +1,4 @@
+import keyword
+print("python_keywords")
+print(keyword.kwlist)
+
