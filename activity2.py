4bc19459460ddd2a200ad92beacf4ef0e474e8bc
Rishit886 <rishitsaharia@gmail.com>
2026-07-07 09:46:21 -0400

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,15 @@
+friends = {
+    "Rishit": "9 March",
+    "Ayushman": "15 July",
+    "Ananya": "22 November",
+    "Priya": "5 January",
+    "Harry": "30 September",
+}
+
+name = input("Enter friend's name: ")
+
+birthday = friends.get(name)
+if birthday:
+    print(f"{name}'s birthday is {birthday}.")
+else:
+    print("Friend not found.")
