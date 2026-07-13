57f04a853ac9e498d6a840e4d86568f75677e567
Rishit886 <rishitsaharia@gmail.com>
2026-07-13 13:18:35 -0400

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,8 +1,14 @@
 
-text = str(input("Enter a string: "))
+print("Enter Marks Obtained in 4 Subjects: ")
+math = int(input("maths :"))
+english = int(input("english :"))
+science = int(input("science :"))
+hindi = int(input("hindi :"))
 
-revText = text[::-1] 
-text = revText
+sum = math+english+science+hindi
+print("sum of math,english,science and hindi = ",sum)
 
-print("Reverse of Given String is:")
-print(text)
+perc = (sum/400)*100
+
+print(end="Percentage Mark = ")
+print(perc)
