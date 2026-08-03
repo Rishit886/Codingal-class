36f15620dec0215d303a295cc99490b85d187071
Rishit886 <rishitsaharia@gmail.com>
2026-08-03 15:42:53 -0400

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,24 +1,8 @@
-a = 10
-b = 12
-c = 0
-
-if a and b and c:
-    print("All the numbers have boolean value as True")
-else:
-    print("Atleast one number has boolean value as False")
-
-
-
-a = 10
-b = -10
-c = 0
-  
-if a > 0 or b > 0:
-    print("Either of the number is greater than 0")
-else:
-    print("No number is greater than 0")
-  
-if b < 0 or c == 0:
-    print("Either of the number is greater than 0")
-else:
-    print("No number is greater than 0")
+number = int(input("Please enter a number: "))
+sum = 0
+i = 1
+while i <= number:
+    sum = sum+ i
+    i = i + 1
+    
+print("Sum", sum)
