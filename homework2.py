cfaf9611545462817f76b8d6a487272c8c4ef685
Rishit886 <rishitsaharia@gmail.com>
2026-08-05 10:43:47 -0400

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,5 +1,5 @@
-age = float(input("Enter a number: "))
-if 10<= age <=20:
-    print("You are allowed to be in my class.")
-else:
-    print("You are banned from my class.")
+num = int(input("Enter a number: "))
+count = 0 # count always starts at 0 because we haven't counted any digits yet
+while num !=0:
+    num = num // 10  # remove one digit from the end
+    count += 1       # add 1 to the count each time
