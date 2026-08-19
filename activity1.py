c9a589282a35f07560b7a5710fc2960141013065
Rishit886 <rishitsaharia@gmail.com>
2026-08-19 10:01:38 -0400

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,8 +1,14 @@
-number = int(input("Please enter a number: "))
-sum = 0
-i = 1
-while i <= number:
-    sum = sum+ i
-    i = i + 1
-    
-print("Sum", sum)
+string = input("Enter a string: ")
+character = input("Enter a character to count: ")
+count = 0
+# i=0
+# while i<len(string):
+#     if string[i]==character:
+#         count+=1
+#     i+=1
+#print(count)
+
+for i in string:
+    if i.lower()==character.lower():
+        count+=1
+print(count)
