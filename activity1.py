1b14f369b67871a564a51b04d232809be09b6c87
Rishit886 <rishitsaharia@gmail.com>
2026-08-31 09:23:29 -0400

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,14 +1,5 @@
-string = input("Enter a string: ")
-character = input("Enter a character to count: ")
-count = 0
-# i=0
-# while i<len(string):
-#     if string[i]==character:
-#         count+=1
-#     i+=1
-#print(count)
-
-for i in string:
-    if i.lower()==character.lower():
-        count+=1
-print(count)
+rows = int(input("Enter the number of rows: "))
+for i in range(rows):
+    for j in range(i + 1):
+        print("*", end=" ")
+    print()
