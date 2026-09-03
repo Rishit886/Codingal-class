df3cb966aea322d5d2cb223d19c05bcd60a17974
Rishit886 <rishitsaharia@gmail.com>
2026-09-02 20:26:11 -0400

    Add files via upload

--- homework1.py
+++ homework1.py
@@ -1,5 +1,9 @@
-import math
-r = int(input("Enter the radius of the circle: "))
-def circumference():
-    return 2*math.pi*r
-print(circumference())
+def shut_down():
+    choice = input("Are you sure you want to shut down the system? Yes or No: ")
+    if choice == "Yes":
+        print("Shutting down the system...")
+        # Add code here to perform the shutdown operation
+    elif choice == "No":
+        print("Shutdown cancelled.")
+    else:
+        print("Invalid input. Please enter 'Yes' or 'No'.")
