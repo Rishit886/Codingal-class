1f3105eb1d0adc4cee7cf7a41ea79c2ee57538c0
Rishit886 <rishitsaharia@gmail.com>
2026-09-02 20:11:51 -0400

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,30 +1,16 @@
-#take input from user
-rowSize = int(input("enter the number of rows: "))
-if rowSize%2==0: #conditions
-  halfDiamRow = int(rowSize/2)
-else:
-  halfDiamRow = int(rowSize/2)+1
-space = halfDiamRow-1
-#loop for upper part 
-for i in range(1, halfDiamRow+1): #loop for rows
-  for j in range(1, space+1): #loop for columns
-    print(end=" ")
-  space = space-1
-  num = 1
-  for j in range(2*i-1):
-    print(end=str(num))
-  #incerementing number at each column
-    num = num+1
-  print()
-space = 1
-#loop for lower part
-for i in range(1, halfDiamRow): #loop for rows
-  for j in range(1, space+1):  #loop for columns
-    print(end=" ")
-  space = space+1
-  num = 1
-  for j in range(1, 2*(halfDiamRow-i)):
-    print(end=str(num)) #display result
-  #incerementing number at each column
-    num = num+1
-  print()
+def factorial(x):
+    '''this is a recursive function to find the factorial of an integer'''
+
+    if x == 0:
+        return 1
+    else:
+        #calling function inside a function
+        return x * factorial(x - 1)
+
+# display result
+print(factorial._doc__)
+print("The facotorial of 0 is:", factorial(0))
+print("The facotorial of 1 is:", factorial(1))
+print("The facotorial of 2 is:", factorial(2))
+print("The facotorial of 5 is:", factorial(5))
+print("The facotorial of 10 is:", factorial(10))
