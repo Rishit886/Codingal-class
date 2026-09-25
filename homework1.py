de7df24ca6cd2cff4df2e9ed9cfcc80c65b63b9a
Rishit886 <rishitsaharia@gmail.com>
2026-09-24 20:57:54 -0400

    Add files via upload

--- homework1.py
+++ homework1.py
@@ -1,4 +1,7 @@
-m = 4
-p = 2.50
-print(m-p)
-
+m = int(input("Enter a number: "))
+if m%2 == 0:
+    print("The number is even.")
+elif m%2 == 1:
+    print("The number is odd.")
+else:
+    print("Sorry, that is not a number.")
